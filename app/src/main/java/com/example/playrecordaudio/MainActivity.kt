5c4cec9c2a1c3e4d1f5b6838a2d8796ef5b104ff
Артемий <va2005art@gmail.com>
2020-12-28 14:53:27 +0300

    add head fragment and anim to this

--- app/src/main/java/com/example/playrecordaudio/MainActivity.kt
+++ app/src/main/java/com/example/playrecordaudio/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.playrecordaudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    private var nowFragment: Fragment? = FragmentHead()
+    var main = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shape_head.setOnClickListener {
+            setFragment(FragmentHead())
+            motion_win.transitionToEnd()
+            main = false
+        }
+
+        shape_rec.setOnClickListener {
+         //   motion_win.transitionToEnd()
+          //  main = false
+        }
+    }
+
+    private fun setFragment(fragment: Fragment){
+        val fr = supportFragmentManager
+        val tr = fr.beginTransaction()
+        tr.hide(nowFragment!!)
+        tr.add(R.id.frameLayout,fragment)
+        tr.commit()
+        nowFragment = fragment
+    }
+
+    override fun onBackPressed() {
+        if (!main){
+            motion_win.transitionToStart()
+            Handler().postDelayed({
+                val fr = supportFragmentManager
+                val tr = fr.beginTransaction()
+                tr.hide(nowFragment!!)
+                tr.commit()
+                main = true
+            }, 1100)
+        }else{
+            exitProcess(1)
+        }
     }
 }
